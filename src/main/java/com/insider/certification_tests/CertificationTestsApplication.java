0c5a96d2b0bcd097d7aa44f91421b62579bc5e76
gabrielrigonidev <rigonigabriel12@gmail.com>
2024-02-05 21:24:33 -0300

    Create Spring application

---
+++ src/main/java/com/insider/certification_tests/CertificationTestsApplication.java
@@ -0,0 +1,13 @@
+package com.insider.certification_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insider/certification_tests/CertificationTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insider.certification_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
